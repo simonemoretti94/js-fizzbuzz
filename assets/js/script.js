4c393ecddcae35a8b43094d0159d3a4040af5981
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-27 13:21:00 +0100

    3rd fixed media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,5 @@
 //script
 
-// let p_output;
-
-// p_output = document.querySelector('p#output');
-
-// document.querySelector('form').addEventListener('submit', function(e){
-
-//     const age_input = document.querySelector('input#age');
-    
-//     const age = parseInt(age_input.value);
-    
-//     const km_input = document.querySelector('input#km');
-    
-//     const km = parseFloat(km_input.value);
-    
-//     console.log('age: ',age,' km: ', km);
-    
-//     console.log('age type: ',age.typeof,' km type: ', km.typeof);
-//     p_output.innerHTML = age + ' ' + km;
-    
-//     e.preventDefault();
-
-//     console.log(e);
-// })
-
-
-// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
-// per i multipli di 3 stampi “Fizz” al posto del numero
-// per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
 let vettore = [];
 
 let i;
